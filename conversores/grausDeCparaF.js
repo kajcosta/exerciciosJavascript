3e410d2763136d24d7e67b7bc27c047b98197ab2
Kauany Costa <107109137+kajcosta@users.noreply.github.com>
2025-04-16 10:34:57 -0300

    3

---
+++ conversores/grausDeCparaF.js
@@ -0,0 +1,5 @@
+//Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula deconversão é F ← (9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
+
+let c =  Number(prompt("Qual a temperatura em celcius"))
+let f = (9*c+160)/5
+document.write(`A temperatura em Fahrenheit é ${area.toFixed(2)}`)
