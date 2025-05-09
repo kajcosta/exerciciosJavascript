1ac7b06807b02f6a8b1aa350fab32f89f2b2b5ae
Kauany Costa <107109137+kajcosta@users.noreply.github.com>
2025-05-09 19:31:40 -0300

    11

---
+++ numeros/quadradoDaDiferenca.js
@@ -0,0 +1,5 @@
+//Ler dois inteiros (variáveis A e B) e imprimir o resultado do quadrado da diferença do primeiro valor pelo segundo.
+let a = Number(prompt("Qual o valor do primeiro número?"))
+let b = Number(prompt("Qual o valor do segundo número?"))
+let ab = (a -b)*(a-b)
+alert("O quadrado da diferença de " +  a + " e " +  b + " é " + ab)
