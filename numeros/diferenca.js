8ac1903100de4b91acb205be85d03e97879d7d46
Kauany Costa <107109137+kajcosta@users.noreply.github.com>
2025-05-09 20:15:49 -0300

    16

---
+++ numeros/diferenca.js
@@ -0,0 +1,11 @@
+// Ler dois valores numéricos inteiros e apresentar o resultado da diferença do maior pelo menor valor. 
+let n1 = Number(prompt("Qual o primeiro número?"))
+let n2 = Number(prompt("Qual o segundo número?"))
+     if (n1 > n2) {
+        let diferenca = n1 - n2}
+        
+     else{
+          diferenca = n2 - n1
+          } 
+      
+      alert("A diferença entre o maior e o menor valor é: " +  diferenca)
