104a7783786007b003a908f397d2de2de1c67089
Kauany Costa <107109137+kajcosta@users.noreply.github.com>
2025-05-09 19:59:46 -0300

    14

---
+++ numeros/somaDosQuadrados.js
@@ -0,0 +1,7 @@
+//Elaborar um programa que efetue a leitura de três valores (A, B e C) e apresente como resultado final à soma dos quadrados dos três valores lidos
+
+let a = Number(prompt("Qual o valor de a?"))
+let b = Number(prompt("Qual o valor de b?")) 
+let c = Number(prompt("Qual o valor de c?"))
+let somaQuadrados = (a*a)+(b*b)+(c*c)
+alert("A soma dos quadrados dos valores é: " + somaQuadrados)
